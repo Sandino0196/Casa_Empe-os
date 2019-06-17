cf8fecd99ac14daeb9f38145ad975239d50a56c2
Rodrigo Sandino <rodrigos1696@hotmail.com>
2019-06-17 11:01:14 -0600

    Primer Cambio

--- Casa_Empeños/Form1.Designer.cs
+++ Casa_Empeños/Form1.Designer.cs
@@ -48,18 +48,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(221, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(221, 198);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Password";
             // 
             // textBox2
             // 
@@ -70,21 +70,21 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(201, 288);
+            this.button1.Location = new System.Drawing.Point(209, 287);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "INGRESAR";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(221, 40);
+            this.label3.Location = new System.Drawing.Point(180, 39);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(142, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "Sistema Empeños Anderson ";
             // 
             // Form1
             // 
