8de4115f20ac793e22a520f3207d1ea453ae0470
Rodrigo Sandino <rodrigos1696@hotmail.com>
2019-06-20 10:25:50 -0600

    Nuevo Cambio

--- Casa_Empeños/Form1.Designer.cs
+++ Casa_Empeños/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -71,7 +70,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(85, 302);
+            this.button1.Location = new System.Drawing.Point(148, 302);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -87,21 +86,11 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Sistema Empeños Anderson ";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(192, 302);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(80, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "REGISTRAR";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(362, 380);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -123,7 +112,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
     }
 }
 
