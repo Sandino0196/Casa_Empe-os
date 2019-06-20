7c0afefb2b4a4f59bfa780622aaf66c092d0d9bc
jdfritzsche <johann25fritzsche@gmail.com>
2019-06-20 12:52:34 -0600

    Form de Facturacion 1.0

--- Casa_Empeños/Facturacion.Designer.cs
+++ Casa_Empeños/Facturacion.Designer.cs
@@ -29,37 +29,37 @@
         private void InitializeComponent()
         {
             this.lblNomComp = new System.Windows.Forms.Label();
-            this.lblApeComp = new System.Windows.Forms.Label();
+            this.lblApellidoComp = new System.Windows.Forms.Label();
             this.lblIDComp = new System.Windows.Forms.Label();
             this.lblVendedor = new System.Windows.Forms.Label();
             this.txtNomComp = new System.Windows.Forms.TextBox();
             this.txtApeComp = new System.Windows.Forms.TextBox();
-            this.txtIdComp = new System.Windows.Forms.TextBox();
+            this.txtIDComp = new System.Windows.Forms.TextBox();
             this.txtVendedor = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblNomComp
             // 
             this.lblNomComp.AutoSize = true;
-            this.lblNomComp.Location = new System.Drawing.Point(12, 25);
+            this.lblNomComp.Location = new System.Drawing.Point(12, 20);
             this.lblNomComp.Name = "lblNomComp";
             this.lblNomComp.Size = new System.Drawing.Size(101, 13);
             this.lblNomComp.TabIndex = 0;
             this.lblNomComp.Text = "Nombre Comprador:";
             // 
-            // lblApeComp
+            // lblApellidoComp
             // 
-            this.lblApeComp.AutoSize = true;
-            this.lblApeComp.Location = new System.Drawing.Point(12, 69);
-            this.lblApeComp.Name = "lblApeComp";
-            this.lblApeComp.Size = new System.Drawing.Size(101, 13);
-            this.lblApeComp.TabIndex = 1;
-            this.lblApeComp.Text = "Apellido Comprador:";
+            this.lblApellidoComp.AutoSize = true;
+            this.lblApellidoComp.Location = new System.Drawing.Point(12, 70);
+            this.lblApellidoComp.Name = "lblApellidoComp";
+            this.lblApellidoComp.Size = new System.Drawing.Size(101, 13);
+            this.lblApellidoComp.TabIndex = 1;
+            this.lblApellidoComp.Text = "Apellido Comprador:";
             // 
             // lblIDComp
             // 
             this.lblIDComp.AutoSize = true;
-            this.lblIDComp.Location = new System.Drawing.Point(248, 22);
+            this.lblIDComp.Location = new System.Drawing.Point(234, 23);
             this.lblIDComp.Name = "lblIDComp";
             this.lblIDComp.Size = new System.Drawing.Size(108, 13);
             this.lblIDComp.TabIndex = 2;
@@ -68,7 +68,7 @@
             // lblVendedor
             // 
             this.lblVendedor.AutoSize = true;
-            this.lblVendedor.Location = new System.Drawing.Point(268, 69);
+            this.lblVendedor.Location = new System.Drawing.Point(279, 70);
             this.lblVendedor.Name = "lblVendedor";
             this.lblVendedor.Size = new System.Drawing.Size(56, 13);
             this.lblVendedor.TabIndex = 3;
@@ -76,28 +76,28 @@
             // 
             // txtNomComp
             // 
-            this.txtNomComp.Location = new System.Drawing.Point(128, 19);
+            this.txtNomComp.Location = new System.Drawing.Point(119, 23);
             this.txtNomComp.Name = "txtNomComp";
             this.txtNomComp.Size = new System.Drawing.Size(100, 20);
             this.txtNomComp.TabIndex = 4;
             // 
             // txtApeComp
             // 
-            this.txtApeComp.Location = new System.Drawing.Point(128, 66);
+            this.txtApeComp.Location = new System.Drawing.Point(119, 67);
             this.txtApeComp.Name = "txtApeComp";
             this.txtApeComp.Size = new System.Drawing.Size(100, 20);
             this.txtApeComp.TabIndex = 5;
             // 
-            // txtIdComp
+            // txtIDComp
             // 
-            this.txtIdComp.Location = new System.Drawing.Point(362, 22);
-            this.txtIdComp.Name = "txtIdComp";
-            this.txtIdComp.Size = new System.Drawing.Size(100, 20);
-            this.txtIdComp.TabIndex = 6;
+            this.txtIDComp.Location = new System.Drawing.Point(348, 20);
+            this.txtIDComp.Name = "txtIDComp";
+            this.txtIDComp.Size = new System.Drawing.Size(100, 20);
+            this.txtIDComp.TabIndex = 6;
             // 
             // txtVendedor
             // 
-            this.txtVendedor.Location = new System.Drawing.Point(362, 62);
+            this.txtVendedor.Location = new System.Drawing.Point(346, 67);
             this.txtVendedor.Name = "txtVendedor";
             this.txtVendedor.Size = new System.Drawing.Size(100, 20);
             this.txtVendedor.TabIndex = 7;
@@ -106,14 +106,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 261);
+            this.ClientSize = new System.Drawing.Size(458, 261);
             this.Controls.Add(this.txtVendedor);
-            this.Controls.Add(this.txtIdComp);
+            this.Controls.Add(this.txtIDComp);
             this.Controls.Add(this.txtApeComp);
             this.Controls.Add(this.txtNomComp);
             this.Controls.Add(this.lblVendedor);
             this.Controls.Add(this.lblIDComp);
-            this.Controls.Add(this.lblApeComp);
+            this.Controls.Add(this.lblApellidoComp);
             this.Controls.Add(this.lblNomComp);
             this.Name = "Facturacion";
             this.Text = "Facturacion";
@@ -125,12 +125,12 @@
         #endregion
 
         private System.Windows.Forms.Label lblNomComp;
-        private System.Windows.Forms.Label lblApeComp;
+        private System.Windows.Forms.Label lblApellidoComp;
         private System.Windows.Forms.Label lblIDComp;
         private System.Windows.Forms.Label lblVendedor;
         private System.Windows.Forms.TextBox txtNomComp;
         private System.Windows.Forms.TextBox txtApeComp;
-        private System.Windows.Forms.TextBox txtIdComp;
+        private System.Windows.Forms.TextBox txtIDComp;
         private System.Windows.Forms.TextBox txtVendedor;
     }
 }
